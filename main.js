5557a8c5832fd4b58dfbdb8a9bb626e5e7d763c9
Oleksii Miroshko <miroshko@live.com>
2015-06-29 23:34:16 +0200

    restify

--- main.js
+++ main.js
@@ -1,53 +1,23 @@
-var express = require('express');        // call express
+var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var restify = require('express-restify-mongoose')
+ 
+mongoose.connect('mongodb://localhost/database');
+ 
+var Ornament = require('./models/ornament');
+ 
 var app = express();
-var cors = require('cors');
-var port = process.env.PORT || 8080;
-
-var mongoose   = require('mongoose');
-mongoose.connect('mongodb://localhost:27017');
-var Ornament = require('./models/ornament.js')
-
-function jsonOut(req, res) {
-  return function(err, json) {
-    if (err) {
-      return res.send(err);
-    }
-    res.json(json);
-  }
-}
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride());
+ 
 var router = express.Router();
-router.route('/ornaments')
-  .post(function(req, res) {
-    var ornament = new Ornament();
-    ornament.save(jsonOut(req, res));
-  })
-  .get(function(req, res) {
-    Ornament.find(jsonOut(req, res));
-  });
-
-router.route('/ornaments/:ornamentId')
-  .get(function(req, res) {
-    Ornament.findById(req.params.ornamentId, jsonOut(req, res));
-  })
-  .patch(function(req, res) {
-    Ornament.findById(req.params.ornamentId, function(err, ornament) {
-      if (!ornament) {
-        ornament = new Ornament({id: req.params.ornamentId});
-      }
-
-      ['operations', 'currentOperation'].forEach(function(key) {
-        if (req.params[key])
-          ornament[key] = req.params[key];
-      });
-
-      ornament.save();
-    })
-  });
-
-app.use('/resources', router);
-app.use(cors());
-
-
-app.listen(port);
-console.log("listening on " + port)
+restify.serve(router, Ornament);
+app.use(router);
+ 
+app.listen(3000, function() {
+    console.log("Express server listening on port 3000");
+});
